aaf57344850751d0b4a1a8091e18fb037cd5d6f2
Prithvi Singh <singhprithvi410@gmail.com>
2024-02-26 02:26:47 +0530

    Removed h1 tag from index page

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy; 
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-const dbUrl = process.env.DB_URL //|| 'mongodb://localhost:27017/yelpcamp';
+const dbUrl = process.env.DB_URL || //'mongodb://localhost:27017/yelpcamp';
 
 // MongoDB Connection
 mongoose.connect(dbUrl)
