96ec7928195106b4850063f5e3e2c1893678178c
Prithvi Singh <singhprithvi410@gmail.com>
2024-02-26 01:45:27 +0530

    some more changes

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'Campscout',
+        folder: 'yelpcamp',
         allowedFormats: ['jpeg', 'png', 'jpg'],
     }
 });
